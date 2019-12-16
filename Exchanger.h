348934e5ee3fcf3ffd57c14aa31c29540b8ab17c
anandkulkarnisg <anand.kulkarni.sg@gmail.com>
2019-12-16 20:25:24 +0800

    Anand Kulkarni - 16/12/2019 20:25:24:355 Asia/Singapore(+08,+0800) - Added support for deletion of copy and assignment and removed boost dependency..

--- Exchanger.h
+++ Exchanger.h
@@ -11,19 +11,21 @@
 #ifndef Exchanger_H
 #define Exchanger_H
 
-template<typename T> class Exchanger : private boost::noncopyable
+template<typename T> class Exchanger
 {
-	private:
-		std::unique_ptr<CyclicBarrier> m_preBarrierPtr;						// We attempt to use a Cyclic Barrier to implement an Exchanger item.
-		std::unique_ptr<CyclicBarrier> m_postBarrierPtr;					// We attempt to use a pair of pre and post barriers to sync up exchanging threads [ Always 2 of them ].
-		T m_item1;															// First item of Exchanger.
-		T m_item2;															// Second item of Exchanger.
+    private:
+        std::unique_ptr<CyclicBarrier> m_preBarrierPtr;						// We attempt to use a Cyclic Barrier to implement an Exchanger item.
+        std::unique_ptr<CyclicBarrier> m_postBarrierPtr;					// We attempt to use a pair of pre and post barriers to sync up exchanging threads [ Always 2 of them ].
+        T m_item1;															// First item of Exchanger.
+        T m_item2;															// Second item of Exchanger.
 
-	public:
-		Exchanger();														// Default Constructor.
-		T exchange(const T&);												// Returns the exchanged item.
-		T exchange(const T&, const long&, const TimeUnit& = TimeUnit::MilliSeconds); // Implement a exchange with timeout and exception mechanism.
-		void reset();														// The barriers can go broken due to timeouts. Hence need a reset interface.
+    public:
+        Exchanger();														// Default Constructor.
+        Exchanger(const Exchanger&)=delete;                                 // Copy constructor disabled. 
+        Exchanger& operator=(const Exchanger&)=delete;                      // assignment operator disabled.
+        T exchange(const T&);												// Returns the exchanged item.
+        T exchange(const T&, const long&, const TimeUnit& = TimeUnit::MilliSeconds); // Implement a exchange with timeout and exception mechanism.
+        void reset();														// The barriers can go broken due to timeouts. Hence need a reset interface.
 };
 
 #endif
