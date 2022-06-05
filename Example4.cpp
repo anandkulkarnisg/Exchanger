d6d194b0b5e455078524a3900de8e5d27642a663
anandkulkarnisg <anand.kulkarni.sg@gmail.com>
2022-06-05 09:09:11 +0800

    Anand Kulkarni - 05/06/2022 09:09:11:786 Asia/Singapore(+08,+0800) - commited via autocommit tool.

--- Example4.cpp
+++ Example4.cpp
@@ -7,53 +7,53 @@
 
 using namespace std;
 
-Exchanger<std::string> exchanger;
-std::mutex cout_mutex;
+Exchanger<string> exchanger;
+mutex cout_mutex;
 const long waitMilliSecs = 5000;
 
-void printOut(const std::string& str1, const std::string& str2, const std::thread::id& id)
+void printOut(const string& str1, const string& str2, const thread::id& id)
 {
-	std::unique_lock<std::mutex> lock(cout_mutex);
-	std::cout << str1 << id << str2 << std::endl;
+	unique_lock<mutex> lock(cout_mutex);
+	cout << str1 << id << str2 << endl;
 	lock.unlock();
 }
 
-void testExchanger(const std::string& inputString)
+void testExchanger(const string& inputString)
 {
-	printOut("I am currently running from thread id = ", ".The value i am having initially is = "+inputString, std::this_thread::get_id());
-	std::string returnString;
+	printOut("I am currently running from thread id = ", ".The value i am having initially is = "+inputString, this_thread::get_id());
+	string returnString;
 	bool isExceptionState = false;
 	try
 	{
 		if(inputString == "Second Thread")
 		{
-			printOut("I am currently running from thread id = ", ".I am going to exchange with wait for =  " + std::to_string(waitMilliSecs), std::this_thread::get_id());
+			printOut("I am currently running from thread id = ", ".I am going to exchange with wait for =  " + to_string(waitMilliSecs), this_thread::get_id());
 			returnString = exchanger.exchange(inputString, waitMilliSecs, TimeUnit::MilliSeconds);
 		}
 		else
 		{
-			printOut("I am currently running from thread id = ", ".I am going to wait for before attempting exchange" + std::to_string(waitMilliSecs), std::this_thread::get_id());
+			printOut("I am currently running from thread id = ", ".I am going to wait for before attempting exchange" + to_string(waitMilliSecs), this_thread::get_id());
 			// This hit about 50 micro seconds earlier than the other Second Thread is about to time out waiting. It makes it sometimes and doesnt sometimes!
-			std::this_thread::sleep_for(std::chrono::microseconds((waitMilliSecs*1000)-50));
+			this_thread::sleep_for(chrono::microseconds((waitMilliSecs*1000)-50));
 			returnString = exchanger.exchange(inputString);
 		}
 	}
-	catch(const std::exception& e)
+	catch(const exception& e)
 	{
-		printOut("Recieved the exception in thread id = ", ". Exception Message is = " + std::string(e.what()), std::this_thread::get_id());
+		printOut("Recieved the exception in thread id = ", ". Exception Message is = " + string(e.what()), this_thread::get_id());
 		isExceptionState = true;
 	}
 	if(!isExceptionState)
-		printOut("I am currently running from thread id = ", ".The value i am having now is = " + returnString, std::this_thread::get_id());
+		printOut("I am currently running from thread id = ", ".The value i am having now is = " + returnString, this_thread::get_id());
 }
 
 int main(int argc, char* argv[])
 {
-	std::string firstString = "First Thread";
-	std::string secondString = "Second Thread";
+	const string firstString = "First Thread";
+	const string secondString = "Second Thread";
 
-	std::thread t1(&testExchanger, firstString);
-	std::thread t2(&testExchanger, secondString);
+	thread t1(&testExchanger, cref(firstString));
+	thread t2(&testExchanger, cref(secondString));
 
 	t1.join();
 	t2.join();		
